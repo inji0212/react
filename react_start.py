e78300234ac0bc356b48d9f71440cc63dca808eb
inji0212 <in17100170@kakao.com>
2024-02-02 11:40:20 +0900

    리액트 시작 설명

---
+++ react_start.py
@@ -0,0 +1,18 @@
+# 폴더를 생성한 후 
+# new terminal클릭
+# npx create-react-app ./ 현재 디렉토리에서 리액트 설치
+
+# npm run start 터미널에서 치면 리액트 실행
+
+# 이름 수정하면 안되는 파일은
+# public/index.html 
+#src/index.js
+#src에 js 랑 css 넣으면 된다. src만 작동하므로
+
+# npm run start 리액트 앱 실행
+# npm run build 빌드 폴더 안에 개발안 코드들이 웹팩을 통해 빌드
+# npm run test 리엑트 앱을 테스트 
+
+# spa 싱글 적인 html 이었는데 현재는 js통해 하나의 index.html 페이지 전환(브라우징)
+# HTML5의 history API를 이용
+# ex) History.back() 뒤페이지로 이동 ..
